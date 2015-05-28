8be9850eddb466b15edb85f9a08ef02a4a65a6fc
alkarps <Tantalalas1@ya.ru>
2015-05-28 17:30:42 +0300

    Исправлены ошибки в mail: корректная обработка картинок, добавлено импортирование модуля os при отправки картинок. Добавлено поле toView в модуле mail.py - список отображаемых получателей. В threadESSDZ.py добавлена передача списка отображаемых получателей.

--- mail.py
+++ mail.py
@@ -16,7 +16,7 @@ List changes on 20.05.2015:
 '''
 # -*- coding: utf8 -*-
 
-def sent_mail (text, to, subj, server = None, port = None, user_name = None, user_passwd = None, images = None, debug = None):
+def sent_mail (text, to, subj, server = None, port = None, user_name = None, user_passwd = None, images = None, toView = None, debug = None):
     u'''Функция отправки письма.
     На вход принимает следующие значения: to, text, subj [, server, port, user_name, user_passwd].
     Параметры server, port, user_name, user_passwd не обязательны. Передаются через ключи при каждом вызове функции.
@@ -33,18 +33,21 @@ def sent_mail (text, to, subj, server = None, port = None, user_name = None, use
         user_name = default_user;
     if user_passwd is None:
         user_passwd = default_passwd;
+    if toView is None:
+        toView = to;
     # формирование сообщения
     msgRoot = MIMEMultipart('related');
-    msgRoot['Subject'] = subj
-    msgRoot['From'] = user_name
-    msgRoot['To'] = to
-    msgText = MIMEMultipart('alternative')
-    msgText = MIMEText(text, "html", "utf-8")
-    msgText['Content-Type'] = "text/html; charset=utf8"
-    msgRoot.attach(msgText)
+    msgRoot['Subject'] = subj;
+    msgRoot['From'] = user_name;
+    msgRoot['To'] = toView;
+    msgText = MIMEMultipart('alternative');
+    msgText = MIMEText(text, "html", "utf-8");
+    msgText['Content-Type'] = "text/html; charset=utf8";
+    msgRoot.attach(msgText);
     #Аттачим картинки
     if images is not None:
         from email.mime.image import MIMEImage;
+        import os;
         for image in images:
             fp = open(image, 'rb');
             img = MIMEImage(fp.read());
@@ -61,7 +64,7 @@ def sent_mail (text, to, subj, server = None, port = None, user_name = None, use
         s.ehlo();
         s.login(user_name, user_passwd);
         try:
-            s.sendmail(user_name, to, msgRoot.as_string());
+            s.sendmail(user_name, to.split(','), msgRoot.as_string());
         finally:
             s.close();
     except Exception, exc:
@@ -75,6 +78,7 @@ def main():
     arg_parser.add_argument('to', type=str, help="Email'ы получателей");
     arg_parser.add_argument('subj', type=str, help='Тема сообщения.');
     arg_parser.add_argument('text', type=str, help='Текст сообщения.');
+    arg_parser.add_argument('-tv','--toView', type=str, default=None, help="Email'ы получателей? которые отображаются в письме");
     arg_parser.add_argument('-s','--server', type=str, default=default_server, required=False, help="Адрес сервера почты. Значение по-умолчанию: %(default)s");
     arg_parser.add_argument('-p','--port', type=int, default=default_port, required=False, help="Порт сервера почты. Значение по-умолчанию: %(default)s");
     arg_parser.add_argument('-u','--user', type=str, default=default_user, required=False, help="Email отправителя, а так же логин для авторизации на сервере почты. Значение по-умолчанию: %(default)s");
@@ -82,7 +86,7 @@ def main():
     arg_parser.add_argument('-i','--images', type=str, default=None, required=False, help="Адрес картинок для добавления в письмо.");
     arg_parser.add_argument('-d','--debug', type=str, default=None, required=False, help="Включить дебаг-режим при отправки письма.");
     args = vars(arg_parser.parse_args());
-    sent_mail(args['text'], args['to'], args['subj'], args['server'], args['port'], args['user'], args['user_passwd'], args['images'], args['debug']);
+    sent_mail(args['text'], args['to'], args['subj'], args['server'], args['port'], args['user'], args['user_passwd'], args['images'].split(','), args['debug'], args['toView']);
     
 import mailsettings as ms;
 default_server = ms.default_server;
--- threadESSDZ.py
+++ threadESSDZ.py
@@ -5,32 +5,38 @@ import mail;
 import graphicsutil;
 from multiprocessing import Process, Manager;
 
+
 def getESSDZStatByTNS(dicTable, setting):
-    logging.basicConfig(filename='ESSDZ_stat_log.log', format = u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s', level = logging.DEBUG);
+    logging.basicConfig(filename='ESSDZ_stat_log.log',
+                        format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s',
+                        level=logging.DEBUG);
     login = setting[0];
     password = setting[1];
     host = setting[2];
     port = setting[3];
     sid = setting[4];
     name = setting[5];
-    logging.info("Start formatting zubaTable for "+name);
+    logging.info("Start formatting zubaTable for " + name);
     result = [];
-    #Собирание строки для таблицы Зубарева
+    # Собирание строки для таблицы Зубарева
     listQueryForZubaTable = ["""select nvl(sum(decode(processingstatus, 'S', 1, 0)),0) "Success",
    nvl(sum(decode(processingstatus, 'E', 1, 0)),0) "Error",
    nvl(sum(decode(processingstatus, 'W', 1, 0)),0) "Wait",
    nvl(sum(decode(processingstatus, 'D', 1, 0)),0) "Deleted"
     from mdm_events where processingstatus !='N' and lastprocessingtime between trunc(SYSDATE-1/24,'HH24') and trunc(SYSDATE,'HH24') -1/86400
-    ""","""select count(1) "New_all" from mdm_events where operationtime between trunc(SYSDATE-1/24,'HH24') and trunc(SYSDATE,'HH24') -1/86400""","""select count(1) "New_processed" from mdm_events where operationtime between trunc(SYSDATE-1/24,'HH24') and trunc(SYSDATE,'HH24') -1/86400 and lastprocessingtime  between trunc(SYSDATE-1/24,'HH24') and trunc(SYSDATE,'HH24') -1/86400""","""select count(1) "All_processed" from mdm_events where lastprocessingtime  between trunc(SYSDATE-1/24,'HH24') and trunc(SYSDATE,'HH24') -1/86400"""];
+    """,
+                             """select count(1) "New_all" from mdm_events where operationtime between trunc(SYSDATE-1/24,'HH24') and trunc(SYSDATE,'HH24') -1/86400""",
+                             """select count(1) "New_processed" from mdm_events where operationtime between trunc(SYSDATE-1/24,'HH24') and trunc(SYSDATE,'HH24') -1/86400 and lastprocessingtime  between trunc(SYSDATE-1/24,'HH24') and trunc(SYSDATE,'HH24') -1/86400""",
+                             """select count(1) "All_processed" from mdm_events where lastprocessingtime  between trunc(SYSDATE-1/24,'HH24') and trunc(SYSDATE,'HH24') -1/86400"""];
     zubaRowTable = utils.getOneRowTableByListQuery(login, password, host, port, sid, listQueryForZubaTable, name);
     result.append(zubaRowTable);
-    logging.info("Start formatting errorTable for "+name);
-    #Собирание таблицы статистики ошибок
+    logging.info("Start formatting errorTable for " + name);
+    # Собирание таблицы статистики ошибок
     listQuery = ["""SELECT  e.entitytypeid, me.name, e.operationtype, e.processingstatus, e.error_descr, count(*)
     FROM mdm_events e join mdm_entities me on e.entitytypeid=me.entitytypeid
     where e.entitytypeid not in (122,117,118,119,125,126,128) and e.processingstatus = 'E' and
     e.LASTPROCESSINGTIME BETWEEN trunc(SYSDATE-1/24,'HH24') and trunc(SYSDATE,'HH24')-1/86400
-    group by e.entitytypeid, me.name, e.operationtype, e.processingstatus, e.error_descr order by e.entitytypeid, e.operationtype, e.processingstatus""",]
+    group by e.entitytypeid, me.name, e.operationtype, e.processingstatus, e.error_descr order by e.entitytypeid, e.operationtype, e.processingstatus""", ]
     statTables = utils.getStatByListQuery(login, password, host, port, sid, listQuery);
     stat = """<table>
     <tr>
@@ -43,7 +49,7 @@ def getESSDZStatByTNS(dicTable, setting):
     </tr>""";
     stat = stat + statTables[0] + "</table>";
     result.append(stat);
-    logging.info("Start formatting points for "+name);
+    logging.info("Start formatting points for " + name);
     graphs = utils.getPointsForGraphicsByListQuery(login, password, host, port, sid, ["""with temp_e as
         (select to_number(to_char(lastprocessingtime,'MI')) mi, count(1) cnt
          from mdm_events
@@ -66,39 +72,43 @@ def getESSDZStatByTNS(dicTable, setting):
          connect by level <= 60) t
     left join temp_e e
       on t.mi = e.mi
-    order by t.mi"""], ['all processed per minute','all new per minute']);
-    pic = '<img src="cid:'+name+'_pic.png" alt="график зависимости количества обработанных за минуту за последний час">';
+    order by t.mi"""], ['all processed per minute', 'all new per minute']);
+    pic = '<img src="cid:' + name + '_pic.png" alt="график зависимости количества обработанных за минуту за последний час">';
     result.append(pic);
-    logging.info("Start formatting graphics for "+name);
-    picname = name+'_pic.png'
-    graphicsutil.createGraphicByPoints(graphics = graphs, fileName = picname,lableX=u"minute",lableY=u"Count per minute", minX = 0, maxX = 59);
-    logging.info("Add to dic for "+name);
-    #Передача в основной поток
+    logging.info("Start formatting graphics for " + name);
+    picname = name + '_pic.png'
+    graphicsutil.createGraphicByPoints(graphics=graphs, fileName=picname, lableX=u"minute", lableY=u"Count per minute",
+                                       minX=0, maxX=59);
+    logging.info("Add to dic for " + name);
+    # Передача в основной поток
     if tuple(result) not in dicTable: dicTable[name] = tuple(result);
-    logging.info("Finish getESSDZStatByTNS for "+name);
+    logging.info("Finish getESSDZStatByTNS for " + name);
 
 
 if __name__ == '__main__':
     import os;
     import settingfile;
-    os.putenv('ORACLE_HOME','/home/coder/rcuHome/');
-    #Список настроек подключений к БД. В картежах параметры подключения в седующем порядке: логин, пароль, хост, порт, сид, название региона (может быть на русском)
+
+    os.putenv('ORACLE_HOME', '/home/coder/rcuHome/');
+    # Список настроек подключений к БД. В картежах параметры подключения в седующем порядке: логин, пароль, хост, порт, сид, название региона (может быть на русском)
     processList = [];
-    logging.basicConfig(filename='ESSDZ_stat_log.log', format = u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s', level = logging.DEBUG);
+    logging.basicConfig(filename='ESSDZ_stat_log.log',
+                        format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s',
+                        level=logging.DEBUG);
     logging.info("Start getting statistic");
     manager = Manager();
     dicTable = manager.dict();
-    #Запускаем на каждую БД свой поток.
+    # Запускаем на каждую БД свой поток.
     logging.info("Start threads");
     for setting in settingfile.settingList:
-        process = Process(target=getESSDZStatByTNS, args=(dicTable,setting));
+        process = Process(target=getESSDZStatByTNS, args=(dicTable, setting));
         processList.append(process);
         process.start();
     for process in processList:
         process.join();
     logging.info("Finish threads");
     logging.info("Start building text mail");
-    #Собираем текст письма из результатов
+    # Собираем текст письма из результатов
     emailText = """<html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
@@ -141,12 +151,12 @@ if __name__ == '__main__':
     path = os.getcwd();
     for key in keys:
         emailText = emailText + dicTable[key][0];
-        errorTables = errorTables + "\n <h2>" + key + "</h2>\n"+dicTable[key][1] + "<br>" +dicTable[key][2];
+        errorTables = errorTables + "\n <h2>" + key + "</h2>\n" + dicTable[key][1] + "<br>" + dicTable[key][2];
         pics.append(os.path.normpath(path + '//' + key + '_pic.png'));
     print pics;
     emailText = emailText + "</table>\n" + errorTables + "</body></html>";
-    #print emailText;
+    # print emailText;
     logging.info("Finish building text mail");
     logging.info("Start sending mail");
-    mail.sent_mail(text = emailText, to = settingfile.to, subj = settingfile.subject, images = pics);
+    mail.sent_mail(text=emailText, to=settingfile.to, subj=settingfile.subject, images=pics, toView=settingfile.toView);
     logging.info("Finish sending mail");
